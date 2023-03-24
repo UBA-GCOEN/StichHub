56563768f03f89b0679c7baa071a845ac0d75da2
Harshal Lade <harshallade2@gmail.com>
2023-03-24 22:16:52 +0530

    Customerlanding1

--- client/StichHub/tailwind.config.cjs
+++ client/StichHub/tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#130F26"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
