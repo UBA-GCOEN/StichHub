fe568b1682cf11507f260361cddb28290decffea
Siddhant Patil <shimpli.patil2804@gmail.com>
2023-04-07 05:32:18 +0530

    testing ar5

--- client/StichHub/src/pages/ThreeDConfigurator.jsx
+++ client/StichHub/src/pages/ThreeDConfigurator.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { ControlsMenu, ClothModel, Navbar } from "../components/Configurator";
-import { ConfiguratorProvider, useCCustomization } from "../contexts/Configurator";
+import {
+  ConfiguratorProvider,
+  useCCustomization,
+} from "../contexts/Configurator";
 
 const ThreeDConfigurator = () => {
-  const {env} = useCCustomization();
+  const { env } = useCCustomization();
 
   return (
-    <div className={`${(env === true) ? "bg-transparent" : "bg-primary"} h-[100vh] overflow-hidden`}>
-      <ConfiguratorProvider>
+    <ConfiguratorProvider>
+      <div
+        className={`${
+          env === true ? "bg-transparent" : "bg-primary"
+        } h-[100vh] overflow-hidden`}
+      >
         <Navbar />
         <ClothModel />
         <ControlsMenu />
-      </ConfiguratorProvider>
-    </div>
+      </div>
+    </ConfiguratorProvider>
   );
 };
 
