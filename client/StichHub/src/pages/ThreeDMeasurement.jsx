a0745d911b69da69d4df45990a70fd57a78d6338
Vishal Suresh Kesharwani <vishal10kesharwani@gmail.com>
2023-04-12 09:41:19 -0600

    Tailor Profile All

--- client/StichHub/src/pages/ThreeDMeasurement.jsx
+++ client/StichHub/src/pages/ThreeDMeasurement.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Profile from '../components/TailorDashboard/Profile'
+import NewTailor from '../components/TailorDashboard/NewTailor'
 
 const ThreeDMeasurement = () => {
   return (
     <div className='bg-'>
-      <Profile/>
+      <NewTailor/>
     </div>
   )
 }
