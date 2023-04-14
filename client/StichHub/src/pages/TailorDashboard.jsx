f2d2c1798f86111bb09ae03f17fb372294b22a32
Naresh <chandanbatven@gmail.com>
2023-04-15 02:04:18 +0530

    made ongoing page and made ongoing page responsive

--- client/StichHub/src/pages/TailorDashboard.jsx
+++ client/StichHub/src/pages/TailorDashboard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import LeftView from "../components/TailorDashboard/LeftView";
-import TopDisplays from "../components/TailorDashboard/TopDisplays";
+import LeftView from "../components/TailorDashboard/main/LeftView";
+import TopDisplays from "../components/TailorDashboard/main/TopDisplays";
 import gradient from "../assets/TailorDashboard/gradient_blue.png";
-import MainDisplay from "../components/TailorDashboard/MainDisplay";
+import MainDisplay from "../components/TailorDashboard/main/MainDisplay";
 import TopProfile from "../components/TailorDashboard/mobile/Main/TopProfile";
 import TopMobileDisplays from "../components/TailorDashboard/mobile/Main/TopMobileDisplays";
 import MainMobileDisplay from "../components/TailorDashboard/mobile/Main/MainMobileDisplay";
+import NavBar from "../components/TailorDashboard/main/NavBar";
+import FooterNavigation from "../components/TailorDashboard/mobile/FooterNavigation";
+import OngoingOrders from "../components/TailorDashboard/ongoing/OngoingOrders";
+import OngoingOrdersOpen from "../components/TailorDashboard/ongoing/OngoingOrdersOpen";
 
-import NavBar from "../components/TailorDashboard/NaBar";
+
 const TailorDashboard = () => {
   return (
     <div className="w-[100vw]">
@@ -15,8 +19,11 @@ const TailorDashboard = () => {
         <NavBar />
         <div className="grid grid-rows-3  grid-flow-col grid-cols-[13%] text-5xl text-center select-none bg-primary - w-[100vw]  h-[92.7vh]">
           <LeftView></LeftView>
-          <TopDisplays></TopDisplays>
+
+          {/* <TopDisplays></TopDisplays>
           <MainDisplay></MainDisplay>
+          <OngoingOrders></OngoingOrders> */}
+          <OngoingOrdersOpen></OngoingOrdersOpen>
         </div>
       </div>
       <div className="lg:hidden">
@@ -27,6 +34,7 @@ const TailorDashboard = () => {
           <TopProfile className="place-self-center"></TopProfile>
           <TopMobileDisplays />
           <MainMobileDisplay></MainMobileDisplay>
+          <FooterNavigation></FooterNavigation>
         </div>
       </div>
     </div>
