b34fdc6ba19e3ae875182c3b190dda24aa458a11
Naresh <chandanbatven@gmail.com>
2023-04-22 04:48:18 +0530

    resolved conflicts

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import Stripe from 'stripe';
-const stripe = new Stripe('sk_test_51Mz6jOSCq6ky7yUKRZzffjTHF8uOQl3qLTY6tlPHDwQg7uP8cMCigqfXVCeodLcdjCbGvZji5fQQ2ZtldrpmctvD00q4zLZHsl');
 
 const router = express.Router();
 
