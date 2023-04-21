461a03d43065721a1126a8d0b96ac8c46391ad14
Siddhant Patil <shimpli.patil2804@gmail.com>
2023-04-21 10:24:56 +0530

    orders logic done

--- client/StichHub/src/App.jsx
+++ client/StichHub/src/App.jsx
@@ -22,6 +22,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Category } from "./components/Home";
 import NewTailor from "./components/TailorDashboard/NewTailor";
 import Profile from "./components/TailorDashboard/Profile";
+import { HomeProvider, useHCustomization } from "./contexts/Home";
+import OrderConfirmation from "./components/Home/OrderConfirmation";
 
 function App() {
   const [userCustomer, setUserCustomer] = useState(
@@ -38,86 +40,93 @@ function App() {
 
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <Routes>
-        {/* Landing Page */}
-        {userCustomer ? (
+      <HomeProvider>
+        <Routes>
+          {/* Landing Page */}
+          {userCustomer ? (
+            <Route
+              path="/"
+              element={
+                !userCustomer ? <MainLandingPage /> : <Navigate to="/home" />
+              }
+            />
+          ) : (
+            <Route
+              path="/"
+              element={
+                !userTailor ? (
+                  <MainLandingPage />
+                ) : (
+                  <Navigate to="/TailorProfileVerification" />
+                )
+              }
+            />
+          )}
+
+          {/* Authentication */}
+          {userCustomer ? (
+            <Route
+              path="/auth"
+              element={
+                !userCustomer ? <LoginSignUp /> : <Navigate to="/home" />
+              }
+            />
+          ) : (
+            <Route
+              path="/auth"
+              element={
+                !userTailor ? (
+                  <LoginSignUp />
+                ) : (
+                  <Navigate to="/TailorProfileVerification" />
+                )
+              }
+            />
+          )}
           <Route
-            path="/"
-            element={
-              !userCustomer ? <MainLandingPage /> : <Navigate to="/home" />
-            }
+            path="/auth/customer"
+            element={!userCustomer ? <CustomerAuth /> : <Navigate to="/home" />}
           />
-        ) : (
           <Route
-            path="/"
+            path="/auth/tailor"
             element={
               !userTailor ? (
-                <MainLandingPage />
+                <AuthTailor />
               ) : (
                 <Navigate to="/TailorProfileVerification" />
               )
             }
           />
-        )}
 
-        {/* Authentication */}
-        {userCustomer ? (
+          {/* Customer Side */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/TailorDetails" element={<TailorDetails />} />
+          <Route path="/home/category" element={<Category />} />
+          <Route path="/Configurator" element={<ThreeDConfigurator />} />
+          <Route path="/measurement" element={<ThreeDMeasurement />} />
+          <Route path="/measurement/" element={<ThreeDMeasurement />} />
+          <Route path="/FabricModel" element={<FabricModel />} />
+          <Route path="/OrderDetails" element={<OrderConfirmation />} />
+
+          <Route path="/Cart" element={<Cart />} />
+          {/* Customer Features  */}
+          <Route path="/ClothesCategory" element={<ClothesCategory />} />
+          <Route path="/CustomersPage" element={<CustomerLandingPage />} />
+
+          {/* Tailor Side */}
+          <Route path="/TailorDashboard" element={<TailorDashboard />} />
+          <Route path="/TailorDashboard/Profile" element={<Profile />} />
+          <Route path="/TailorDashboard/NewTailor" element={<NewTailor />} />
           <Route
-            path="/auth"
-            element={!userCustomer ? <LoginSignUp /> : <Navigate to="/home" />}
+            path="/TailorProfileVerification"
+            element={<TailorProfileVerification />}
           />
-        ) : (
-          <Route
-            path="/auth"
-            element={
-              !userTailor ? (
-                <LoginSignUp />
-              ) : (
-                <Navigate to="/TailorProfileVerification" />
-              )
-            }
-          />
-        )}
-        <Route
-          path="/auth/customer"
-          element={!userCustomer ? <CustomerAuth /> : <Navigate to="/home" />}
-        />
-        <Route
-          path="/auth/tailor"
-          element={
-            !userTailor ? (
-              <AuthTailor />
-            ) : (
-              <Navigate to="/TailorProfileVerification" />
-            )
-          }
-        />
 
-        {/* Customer Side */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/TailorDetails" element={<TailorDetails />} />
-        <Route path="/home/category" element={<Category />} />
-        <Route path="/Configurator" element={<ThreeDConfigurator />} />
-        <Route path="/measurement" element={<ThreeDMeasurement />} />
-        <Route path="/FabricModel" element={<FabricModel />} />
-        <Route path="/Cart" element={<Cart />} />
-        {/* Customer Features  */}
-        <Route path="/ClothesCategory" element={<ClothesCategory />} />
-        <Route path="/CustomersPage" element={<CustomerLandingPage />} />
-
-        {/* Tailor Side */}
-        <Route path="/TailorDashboard" element={<TailorDashboard />} />
-        <Route path="/TailorDashboard/Profile" element={<Profile />} />
-        <Route path="/TailorDashboard/NewTailor" element={<NewTailor />} />
-        <Route
-          path="/TailorProfileVerification"
-          element={<TailorProfileVerification />}
-        />
-
-        {/* Miscelleneous */}
-        <Route path="/FabricSelection" element={<FabricSelection />} />
-        <Route path="/HomeOnlineVisit" element={<HomeOnlineVisit />} />
-      </Routes>
+          {/* Miscelleneous */}
+          <Route path="/FabricSelection" element={<FabricSelection />} />
+          <Route path="/HomeOnlineVisit" element={<HomeOnlineVisit />} />
+        </Routes>
+      </HomeProvider>
     </GoogleOAuthProvider>
   );
 }
---
+++ server/controllers/order.js
@@ -0,0 +1,80 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+import OrderList from "../models/order.js";
+
+export const addOrder = async (req, res) => {
+  const order = req.body;
+  try {
+    const isTailor = await OrderList.find({ tailorId: order.tailorId });
+
+    // console.log(isTailor);
+
+    if (isTailor.length > 0) {
+      await OrderList.updateOne(
+        { _id: isTailor[0]._id },
+        {
+          $push: {
+            requests: {
+              customerId: req.userId,
+              orderData: order,
+              status: "pending",
+            },
+          },
+        }
+      );
+      res.status(201).json({ message: "Success!! Request saved" });
+    } else {
+      const newOrder = new OrderList({
+        tailorId: order.tailorId,
+        requests: {
+          customerId: req.userId,
+          orderData: order,
+          status: "pending",
+        },
+        createdAt: new Date().toISOString(),
+      });
+      await newOrder.save();
+      res.status(201).json({ message: "Success!! Request saved" });
+    }
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+    console.log(error);
+  }
+};
+
+export const getOrders = async (req, res) => {
+  const tailorId = req.userId;
+
+  try {
+    const orders = await OrderList.find({ tailorId: tailorId });
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const updateStatus = async (req, res) => {
+  try {
+    const { orderId, requestId } = req.params;
+    const { status } = req.body;
+
+    const order = await OrderList.findById(orderId);
+    const request = order.requests.id(requestId);
+
+    request.status = status;
+
+    await order.save();
+
+    res.status(200).send({ message: "Order status updated successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Failed to update order status" });
+  }
+};
+
+export default router;
