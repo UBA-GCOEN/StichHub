fbd0483a5ada0fb5a54a39d8a1191a18197a6334
Naresh <chandanbatven@gmail.com>
2023-04-22 04:43:26 +0530

    resolved conflicts

--- server/index.js
+++ server/index.js
@@ -4,98 +4,15 @@ import mongoose from "mongoose";
 import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config();
-import Razorpay from 'razorpay';
-
-
 import test from "./routes/test.js";
-import userTailorRouter from "./routes/userTailor.js";
-import userCustomerRouter from "./routes/userCustomer.js";
-import tailorsDetails from "./routes/tailorsDetails.js";
-import orders from "./routes/orders.js";
 
 const app = express();
+
+app.use("/test", test);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Welcome to StichHub Server!");
-});~
-app.use("/test", test);
-app.use("/userTailor", userTailorRouter);
-app.use("/userCustomer", userCustomerRouter);
-app.use("/tailors", tailorsDetails);
-app.use("/order", orders);
-
-
-app.post('/api/form-submit', async (req, res) => {
-  const formData = req.body;
-  console.log(formData);
-
-
-      // working need to be recode
-    // const amount = 1000; 
-    // const currency = 'inr';
-    // const paymentMethod = await stripe.paymentMethods.create({
-    //   type: 'card',
-    //   card: {
-    //     number: '4242424242424242',
-    //     exp_month: 12,
-    //     exp_year: 2025,
-    //     cvc: '123',
-    //   },
-    // });
-    
-    // const paymentMethodId = paymentMethod.id;
-    
-    
-    // const paymentIntent = await stripe.paymentIntents.create({
-    //   amount,
-    //   currency,
-    //   payment_method: paymentMethodId,
-    //   confirm: true,
-    // });
-    // res.json({ success: true });
-    // console.log(paymentIntent);
-  
-    
-
-
-
-  
-  // Do something with the form data here
-  
-  res.send('Form submission successful!');
-});
-
-
-
-app.get('/create-order', async (req, res) => {
-  const razorpay = new Razorpay({
-    key_id: client_Id,
-    key_secret: secret
-  });
-
-  const amount = 1000;
-const currency = 'INR';
-const options = {
-  amount: amount,
-  currency: currency
-};
-
-  razorpay.orders.create(options, (error, order) => {
-    if (error) {
-      console.error(error);
-      // Handle error
-    } else {
-      console.log(order);
-      // Redirect user to Razorpay checkout page
-      res.redirect(order["checkout_url"]);
-    }
-  });
-});
-
 
 const CONNECTION_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;
