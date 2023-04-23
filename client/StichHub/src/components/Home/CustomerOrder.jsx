42a95f1e3b709e48fb05326c03aee2ca58e7d139
Vishal Suresh Kesharwani <vishal10kesharwani@gmail.com>
2023-04-23 17:38:51 -0600

    orderconfirmation and customerorder page completed

--- client/StichHub/src/components/Home/CustomerOrder.jsx
+++ client/StichHub/src/components/Home/CustomerOrder.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "../../axios.js";
 import { Player } from "@lottiefiles/react-lottie-player";
+import Navbar from "./Navbar.jsx";
+import Footer from "../MainLandingPage/Footer.jsx";
+import OrderConfirmation from "./OrderConfirmation.jsx";
 
 const CustomerOrder = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
@@ -24,66 +27,128 @@ const CustomerOrder = () => {
     }
   };
 
-  const handleSubmit = async(e, order, tailorId, orderId) => {
+  const handleSubmit = async (e, order, tailorId, orderId) => {
     setIsLoading(true);
     try {
       console.log(order);
-      await axios.post(`/cart/${orderId}/${tailorId}`, order)
+      await axios.post(`/cart/${orderId}/${tailorId}`, order);
       setIsLoading(false);
-      navigateTo('/Cart');
+      navigateTo("/Cart");
     } catch (error) {
       console.error(error.message);
       setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     getOrder();
   }, [1]);
 
+  const [state, setState] = useState();
+
   return (
-    <div className="mx-5 my-5 flex flex-wrap gap-3 sm:gap-7">
-      {isLoading ? (
-        <div className="relative">
-          <div className="absolute z-[100] left-[-10vw] lg:left-[30vw] top-[10vh]">
-            <Player
-              src="https://assets2.lottiefiles.com/packages/lf20_a0liqnwx.json"
-              background="transparent"
-              speed="1"
-              style={{ height: "500px", width: "500px" }}
-              loop
-              autoplay
-            />
+    <div>
+      <Navbar />
+
+      <div className="mx-5 my-5 flex flex-wrap gap-3 sm:gap-7">
+        {isLoading ? (
+          <div className="relative">
+            <div className="absolute z-[100] left-[-10vw] lg:left-[30vw] top-[10vh]">
+              <Player
+                src="https://assets2.lottiefiles.com/packages/lf20_a0liqnwx.json"
+                background="transparent"
+                speed="1"
+                style={{ height: "500px", width: "500px" }}
+                loop
+                autoplay
+              />
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="max-w-2xl mx-auto pt-16 sm:py-24 sm:px-6 lg:max-w-2xl lg:px-8">
+        <div className="px-4 space-y-2 sm:px-0 sm:flex sm:items-baseline sm:justify-between sm:space-y-0">
+          <div className=" sm:items-baseline sm:space-x-4">
+            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-xl">
+              Your Recent Order Acceptance Status
+            </h1>
+            <p className="text-gray-500 ml-0 text-left ">
+              if your order acceptance status is not changing, then please try
+              to{" "}
+              <button className="text-red-500 hover:text-red-800 hover:underline ">
+                refresh
+              </button>{" "}
+              this page to get status updated!
+            </p>
           </div>
         </div>
-      ) : null}
-      Your Recent Order:
-      {orders.map((items, index) => {
-        return (
-          <div key={index}>
-            <span>tailorId : {items.tailorId}</span>
-            {items.requests.map((order, index2) => {
-              if (order.customerId === user.result._id) {
-                return (
-                  <div key={index2} className="flex flex-col">
-                    <div>{order.orderData.category}</div>
-                    <span>{order.status}</span>
-                    {order.status === "accepted" && !order.order && (
-                      <div>
-                          <button 
-                          className="bg-blue-500 rounded-lg py-2 px-3 w-fit text-white"
-                          onClick={(e) => handleSubmit(e, order, items.tailorId, order._id)}>
-                            Add to Cart
-                          </button>
+        {orders.map((items, index) => {
+          return (
+            <div className="mt-6" key={index}>
+              {items.requests.map((order, index2) => {
+                if (order.customerId === user.result._id) {
+                  return (
+                    <div className="space-y-8" key={index2}>
+                      <div className="bg-white border-t border-b border-gray-200 shadow-sm sm:border sm:rounded-lg">
+                        <div className="py-6 px-4 sm:px-6 lg:grid lg:grid-cols-12 lg:gap-x-8 lg:p-8">
+                          <div className="sm:flex lg:col-span-12">
+                            <div className="flex-shrink-0 w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-none sm:w-40 sm:h-40">
+                              <img
+                                src="https://tailwindui.com/img/ecommerce-images/confirmation-page-04-product-02.jpg"
+                                alt="Off-white t-shirt with circular dot illustration on the front of mountain ridges that fade."
+                                className="object-center object-cover"
+                              />
+                            </div>
+
+                            <div className="mt-6 sm:mt-0 sm:ml-6 text-center lg:text-left">
+                              <h3 className="text-2xl font-bold text-gray-900">
+                                {order.orderData.category}{" "}
+                              </h3>
+                              <p className="mt-3 font-semibold text-sm text-gray-500 ">
+                                Tailor Name: {items.tailorId}
+                              </p>
+                              <p className="mt-2 text-md font-semibold text-gray-900">
+                                ₹35.00
+                              </p>
+                              <p className="mt-2 font-semibold justify-center lg:justify-start text-sm text-gray-500 flex">
+                                Order Status:{" "}
+                                <p className="text-green-500 ml-4">
+                                  {order.status}
+                                </p>
+                              </p>
+
+                              {order.status === "accepted" && !order.order && (
+                                <div>
+                                  <button
+                                    className="bg-blue-500 rounded-lg py-2 px-3 w-fit text-white"
+                                    onClick={(e) =>
+                                      handleSubmit(
+                                        e,
+                                        order,
+                                        items.tailorId,
+                                        order._id
+                                      )
+                                    }
+                                  >
+                                    Add to Cart
+                                  </button>
+                                </div>
+                              )}
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                    )}
-                  </div>
-                );
-              }
-            })}
-          </div>
-        );
-      })}
+
+                      {/* <!-- More products... --> */}
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <Footer />
     </div>
   );
 };
--- client/StichHub/src/components/Home/OrderConfirmation.jsx
+++ client/StichHub/src/components/Home/OrderConfirmation.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useHCustomization } from "../../contexts/Home";
 import axios from "../../axios";
 import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "../MainLandingPage/Footer";
 
 const OrderConfirmation = () => {
   const { orderDetails, setOrderDetails } = useHCustomization();
@@ -18,32 +20,186 @@ const OrderConfirmation = () => {
   };
 
   return (
-    <div className="text-center text-black">
-      OrderConfirmation :<span>{orderDetails.category}</span>
-      <br />
-      {orderDetails.clothDetails.sleeve} <br />
-      {orderDetails.clothDetails.collar} <br />
-      {orderDetails.clothDetails.backDetails} <br />
-      {orderDetails.clothDetails.cuffs} <br />
-      {orderDetails.clothDetails.color} <br />
-      {orderDetails.clothDetails.fabric} <br />
-      {orderDetails.measurements.height} <br />
-      {orderDetails.measurements.weight} <br />
-      {orderDetails.measurements.shoeSize} <br />
-      {orderDetails.measurements.age} <br />
-      {orderDetails.measurements.gender} <br />
-      {orderDetails.measurements.neckSize} <br />
-      {orderDetails.measurements.chestSize} <br />
-      {orderDetails.measurements.shoulderSize} <br />
-      {orderDetails.measurements.waistSize} <br />
-      {orderDetails.measurements.armLength} <br />
-      {orderDetails.measurements.inseam} <br />
-      <button
-        className="py-2 px-3 rounded-xl bg-blue-500 text-white"
-        onClick={handleSubmit}
-      >
-        Request Tailor
-      </button>
+    <div>
+      <Navbar />
+      {/* <div className="text-center text-black">
+        OrderConfirmation :<span>{orderDetails.category}</span>
+        <br />
+        {orderDetails.clothDetails.sleeve} <br />
+        {orderDetails.clothDetails.collar} <br />
+        {orderDetails.clothDetails.backDetails} <br />
+        {orderDetails.clothDetails.cuffs} <br />
+        {orderDetails.clothDetails.color} <br />
+        {orderDetails.clothDetails.fabric} <br />
+        {orderDetails.measurements.height} <br />
+        {orderDetails.measurements.weight} <br />
+        {orderDetails.measurements.shoeSize} <br />
+        {orderDetails.measurements.age} <br />
+        {orderDetails.measurements.gender} <br />
+        {orderDetails.measurements.neckSize} <br />
+        {orderDetails.measurements.chestSize} <br />
+        {orderDetails.measurements.shoulderSize} <br />
+        {orderDetails.measurements.waistSize} <br />
+        {orderDetails.measurements.armLength} <br />
+        {orderDetails.measurements.inseam} <br />
+        <button
+          className="py-2 px-3 rounded-xl bg-blue-500 text-white"
+          onClick={handleSubmit}
+        >
+          Request Tailor
+        </button>
+      </div> */}
+      <div className="shadow-xl">
+        <div className="max-w-2xl mx-auto pt-16 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="px-4 space-y-2 sm:px-0 sm:flex sm:items-baseline sm:justify-between sm:space-y-0">
+            <div className="flex sm:items-baseline sm:space-x-4">
+              <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+                Order Stitching Confirmation
+              </h1>
+            </div>
+            <p className="text-sm text-red-600">
+              Order Approval From Tailor Pending{" "}
+              <time
+                datetime="2021-03-22"
+                className="font-medium text-gray-900"
+              ></time>
+            </p>
+            <a
+              href="#"
+              className="text-sm font-medium text-indigo-600 hover:text-indigo-500 sm:hidden"
+            >
+              View invoice<span aria-hidden="true"> &rarr;</span>
+            </a>
+          </div>
+
+          <div className="mt-6">
+            <div className="space-y-8">
+              <div className="bg-white border-t border-b border-gray-200 shadow-sm sm:border sm:rounded-lg">
+                <div className="py-6 px-4 sm:px-6 lg:grid lg:grid-cols-12 lg:gap-x-8 lg:p-8">
+                  <div className="sm:flex lg:col-span-5">
+                    <div className="flex-shrink-0 w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-none sm:w-40 sm:h-40">
+                      <img
+                        src="https://tailwindui.com/img/ecommerce-images/confirmation-page-04-product-02.jpg"
+                        alt="Off-white t-shirt with circular dot illustration on the front of mountain ridges that fade."
+                        className="object-center object-cover"
+                      />
+                    </div>
+
+                    <div className="mt-6 sm:mt-0 sm:ml-6 text-center lg:text-left">
+                      <h3 className="text-2xl font-bold text-gray-900">
+                        <a href="#">{orderDetails.category}</a>
+                      </h3>
+                      <p className="mt-2 text-md font-semibold text-gray-900">
+                        ₹35.00
+                      </p>
+                      <p className="mt-3 font-semibold text-sm text-gray-500">
+                        Fabric Type: {orderDetails.clothDetails.fabric}
+                      </p>
+                      <p className="mt-3  justify-center lg:justify-start font-semibold text-sm text-gray-500 flex gap-3">
+                        Color:{" "}
+                        <div className="grid grid-flow-col grid-cols-[10%_90%]">
+                          <div
+                            className="rounded-full h-5 w-5 border border-black"
+                            style={{
+                              backgroundColor: orderDetails.clothDetails.color,
+                            }}
+                          ></div>
+
+                          <div className="text-sm ml-4 w-fit justify-self-center font-semibold">
+                            {orderDetails.clothDetails.color}
+                          </div>
+                        </div>
+                      </p>
+                      <button
+                          className=" text-white rounded-md hover:bg-blue-500 mt-4 px-5 py-2 bg-blue-600"
+                          onClick={handleSubmit}
+                        >
+                          Request Tailor
+                        </button>
+                    </div>
+                  </div>
+
+                  <div className="mt-6 lg:mt-0 lg:col-span-7">
+                    <dl className="grid grid-col-1 ml-[15%] mr-[15%] lg:mr-0 divide-y-4 lg:divide-y-0 gap-y-4 text-lg lg:gap-y-0 lg:ml-0  lg:grid-cols-3 gap-x-6 lg:text-sm">
+                      <div>
+                        <dt className="font-semibold text-gray-900">
+                          Body Information
+                        </dt>
+                        <dd className="mt-3 text-black font-medium space-y-1">
+                          <span className="block ">
+                            Height: {orderDetails.measurements.height}
+                          </span>
+                          <span className="block">
+                            Weight: {orderDetails.measurements.weight}
+                          </span>
+                          <span className="block">
+                            Age: {orderDetails.measurements.age}
+                          </span>
+                          <span className="block">
+                            Shoe Size: {orderDetails.measurements.shoeSize}
+                          </span>
+                          <span className="block">
+                            Gender: {orderDetails.measurements.gender}
+                          </span>
+                        </dd>
+                      </div>
+                      <div>
+                        <dt className="font-semibold col-span-2 text-gray-900">
+                          Order Complete Information
+                        </dt>
+                        <dd className="mt-3 text-black font-medium space-y-1">
+                          <span className="block ">
+                            Sleeve: {orderDetails.clothDetails.sleeve}
+                          </span>
+                          <span className="block">
+                            Collar: {orderDetails.clothDetails.collar}
+                          </span>
+                          <span className="block">
+                            Backdetails: {orderDetails.clothDetails.backDetails}
+                          </span>
+                          <span className="block">
+                            Cuffs: {orderDetails.clothDetails.cuffs}
+                          </span>
+                        </dd>
+                      </div>
+                      <div>
+                        <dt className="font-semibold col-span-2 text-gray-900">
+                          Measurement Details
+                        </dt>
+                        <dd className="mt-3 text-black space-y-1">
+                          <p className="font-medium">
+                            Neck Size: {orderDetails.measurements.neckSize}
+                          </p>
+                          <p className="font-medium">
+                            Shoulder Size:{" "}
+                            {orderDetails.measurements.shoulderSize}
+                          </p>
+                          <p className="font-medium">
+                            Waist Size: {orderDetails.measurements.waistSize}
+                          </p>
+                          <p className="font-medium">
+                            Arm Length: {orderDetails.measurements.armLength}
+                          </p>
+                          <p className="font-medium">
+                            Inseam: {orderDetails.measurements.inseam}
+                          </p>
+                          <p className="font-medium">
+                            Chest Size: {orderDetails.measurements.chestSize}
+                          </p>
+                        </dd>
+                        
+                      </div>
+                    </dl>
+                  </div>
+                </div>
+              </div>
+
+              {/* <!-- More products... --> */}
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
