9a4a346da035eedf9b16ba2626975d13fdfd3973
Deepanshu <21bcs072@iiitdmj.ac.in>
2023-05-21 22:47:57 +0530

    Added scroll to top option in some long pages

--- client/StichHub/src/pages/HomeOnlineVisit.jsx
+++ client/StichHub/src/pages/HomeOnlineVisit.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import YourOrders from '../components/Cart/YourOrders'
+import GoToTop from '../components/GoToTop'
 
 const HomeOnlineVisit = () => {
   return (
     <div>
+      <GoToTop/>
       <YourOrders/>
     </div>
   )
