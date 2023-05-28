eebcd07745eb2529c81d6a0cd43039795237c558
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-28 17:27:58 +0530

    #130-signUp page

--- client/StichHub/src/components/Loginsignup/CustomerAuth.jsx
+++ client/StichHub/src/components/Loginsignup/CustomerAuth.jsx
@@ -59,7 +59,7 @@ const CustomerAuth = () => {
     localStorage.setItem(
       "profile",
       JSON.stringify({
-        result
+        result,
       })
     );
 
@@ -75,17 +75,15 @@ const CustomerAuth = () => {
   };
 
   const [passwordType, setPasswordType] = useState("password");
-  
+
   const passwordToggle = () => {
     if (passwordType === "password") {
       setPasswordType("text");
-    }
-    else
-      setPasswordType("password");
+    } else setPasswordType("password");
   };
 
   return (
-    <div className="bg-gray-800 h-[100vh] flex justify-between overflow-hidden">
+    <div className="bg-gray-800 h-[100vh] flex justify-between ">
       {/* Loading Animations */}
       {isLoading ? (
         <div className="relative">
@@ -101,34 +99,64 @@ const CustomerAuth = () => {
           </div>
         </div>
       ) : null}
-      {/* left Side */}
-      <div className="relative bg-primary w-full lg:w-[49vw] my-10 rounded-3xl lg:rounded-r-3xl">
-        <div className="relative z-[5]">
-          {/* logo */}
-          <a href="/" className="flex justify-center mt-10">
-            <img src={logo} className="w-[240px]" />
-          </a>
-          {/* title */}
-          <div className="flex justify-center my-4">
-            <div>
-              <img src={customerimg} alt="" className="w-[50px] mr-5" />
+      <div className="flex justify-between  sm:w-[65%] mx-auto w-full">
+        {/* left Side */}
+        <div className="relative bg-primary w-full  lg:w-[100%] my-10 ">
+          <div className="relative z-[5]">
+            {/* logo */}
+            <a href="/" className="flex justify-center mt-10">
+              <img src={logo} className="w-[240px]" />
+            </a>
+            {/* title */}
+            <div className="flex justify-center my-4">
+              <div>
+                <img src={customerimg} alt="" className="w-[50px] mr-5" />
+              </div>
+              <div className="mt-3 text-center">
+                <span className="text-white text-3xl font-semibold">
+                  Customer {isregister ? "Register" : "Sign in"}
+                </span>
+              </div>
             </div>
-            <div className="mt-3 text-center">
-              <span className="text-white text-3xl font-semibold">
-                Customer {isregister ? "Register" : "Sign in"}
-              </span>
+
+            {/* Auth Error */}
+            <div className="flex justify-center">
+              <p className="text-red-500 m-2 text-center">{error}</p>
             </div>
-          </div>
 
-          {/* Auth Error */}
-          <div className="flex justify-center">
-            <p className="text-red-500 m-2 text-center">{error}</p>
-          </div>
-
-          {/* form */}
-          <div className="flex justify-center">
-            <form onSubmit={handleSumbmit}>
-              {isregister && (
+            {/* form */}
+            <div className="flex  justify-center">
+              <form onSubmit={handleSumbmit}>
+                {isregister && (
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 absolute z-[5] m-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      name="name"
+                      id="name"
+                      value={form.name}
+                      onChange={handleChange}
+                      className="mt-[10px] block w-[300px] py-2 pl-[45px] bg-white border rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                "
+                    />
+                  </div>
+                )}
                 <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -141,159 +169,136 @@ const CustomerAuth = () => {
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
                     />
                   </svg>
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    name="name"
-                    id="name"
-                    value={form.name}
-                    onChange={handleChange}
-                    className="mt-[10px] block w-[300px] py-2 pl-[45px] bg-white border rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
-                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                "
-                  />
-                </div>
-              )}
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 absolute z-[5] m-3"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-                  />
-                </svg>
 
-                <input
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  id="email"
-                  value={form.email}
-                  onChange={handleChange}
-                  className="mt-[10px] block w-[300px] py-2 pl-[45px] bg-white border border-slate-300 rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    id="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    className="mt-[10px] block w-[300px] py-2 pl-[45px] bg-white border border-slate-300 rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                 invalid:border-pink-500 invalid:text-pink-600
                 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-                />
-              </div>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 absolute z-[5] m-3"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
                   />
-                </svg>
-
-                <input
-                  type={passwordType}
-                  placeholder="Password"
-                  name="password"
-                  id="password"
-                  value={form.password}
-                  onChange={handleChange}
-                  className="mt-[10px] block w-[300px] py-2 pl-[45px] pr-[2.3rem] bg-white border border-slate-300 rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
-                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                "
-                />
-                <div onClick={passwordToggle} className="absolute cursor-pointer flex items-center z-[5] mt-[-1.8rem] ml-[17rem]">
-                {passwordType === "password" ? <FiEyeOff /> : <FiEye />}
                 </div>
-
-              </div>
-              {isregister && (
                 <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    strokeWidth={1.5}
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="w-6 h-6 absolute z-[5] m-3"
                   >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                      d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
                     />
                   </svg>
 
                   <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    value={form.confirmPassword}
+                    type={passwordType}
+                    placeholder="Password"
+                    name="password"
+                    id="password"
+                    value={form.password}
                     onChange={handleChange}
                     className="mt-[10px] block w-[300px] py-2 pl-[45px] pr-[2.3rem] bg-white border border-slate-300 rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
                 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
                 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
                 "
                   />
+                  <div
+                    onClick={passwordToggle}
+                    className="absolute cursor-pointer flex items-center z-[5] mt-[-1.8rem] ml-[17rem]"
+                  >
+                    {passwordType === "password" ? <FiEyeOff /> : <FiEye />}
+                  </div>
                 </div>
-              )}
-              <div className="flex justify-center">
-                <button
-                  type="submit"
-                  className="mt-[15px] block w-[170px] py-2 bg-blue-500 text-white rounded-xl font-regular text-xl"
-                >
-                  {isregister ? "Register" : "Sign in"}
-                </button>
-              </div>
+                {isregister && (
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 absolute z-[5] m-3"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                      />
+                    </svg>
 
-              <h1 className="text-center text-white text-xl py-4">or</h1>
+                    <input
+                      type="password"
+                      placeholder="Confirm Password"
+                      name="confirmPassword"
+                      id="confirmPassword"
+                      value={form.confirmPassword}
+                      onChange={handleChange}
+                      className="mt-[10px] block w-[300px] py-2 pl-[45px] pr-[2.3rem] bg-white border border-slate-300 rounded-xl text-xl shadow-sm drop-shadow-lg placeholder-slate-400 text-black focus:font-medium
+                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                "
+                    />
+                  </div>
+                )}
+                <div className="flex flex-col lg:flex-row justify-center">
+                  <button
+                    type="submit"
+                    className="mt-[15px] block w-[170px] m-auto py-2 bg-blue-500 text-white rounded-xl font-regular text-xl"
+                  >
+                    {isregister ? "Register" : "Sign in"}
+                  </button>
 
-              <div className="flex justify-center">
-                <GoogleLogin
-                  onSuccess={googleSuccess}
-                  onFailure={googleError}
-                  cookiePolicy="single_host_origin"
-                />
-              </div>
-              <h1 className="text-center text-white text-md pt-6">
-                {isregister ? "Already a user?" : "Don't have an account?"}
-                <a
-                  className="cursor-pointer text-blue-400"
-                  onClick={switchMode}
-                >
-                  &nbsp; {isregister ? "sign in" : "register"}
-                </a>
-              </h1>
-            </form>
+                  <h1 className="mt-4 text-center text-white text-xl px-2 py-1">
+                    or
+                  </h1>
+
+                  <div className="lg:m-[9px] block w-[170px] mx-auto py-2 text-white rounded-xl font-regular text-xl">
+                    <GoogleLogin
+                      onSuccess={googleSuccess}
+                      onFailure={googleError}
+                      cookiePolicy="single_host_origin"
+                    />
+                  </div>
+                </div>
+
+                <h1 className="text-center text-white text-md pt-1">
+                  {isregister ? "Already a user?" : "Don't have an account?"}
+                  <a
+                    className="cursor-pointer text-blue-400"
+                    onClick={switchMode}
+                  >
+                    &nbsp; {isregister ? "sign in" : "register"}
+                  </a>
+                </h1>
+              </form>
+            </div>
           </div>
+
+          {/* BG-Gradients */}
+          <div className="absolute circleGradient-blue w-[300px] h-[300px] top-[-10px] left-[-100px] z-0 blur-2xl"></div>
+          <div className="absolute circleGradient-peach w-[300px] h-[300px] bottom-[0px] left-[-100px] z-0 blur-2xl"></div>
         </div>
 
-        {/* BG-Gradients */}
-        <div className="absolute circleGradient-blue w-[300px] h-[300px] top-[-10px] left-[-100px] z-0 blur-2xl"></div>
-        <div className="absolute circleGradient-peach w-[300px] h-[300px] bottom-[0px] left-[-100px] z-0 blur-2xl"></div>
-      </div>
-
-      {/* Right Side (img)*/}
-      <div className="hidden lg:flex bg-[url('../src/assets/loginsignupbg.png')] bg-contain bg-no-repeat bg-[#BADDF1] bg-center w-[49vw] my-10 rounded-l-3xl">
-        <img
-          src={shortlogo}
-          className="w-[5vw] absolute bottom-14 right-5"
-        ></img>
+        {/* Right Side (img)*/}
+        <div className="hidden lg:flex bg-[url('../src/assets/loginsignupbg.png')] bg-contain bg-no-repeat bg-[#BADDF1] bg-center w-[100%] my-10 ">
+          <img
+            src={shortlogo}
+            className="w-[5vw] absolute bottom-14 right-5"
+          ></img>
+        </div>
       </div>
     </div>
   );
