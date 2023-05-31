4e7b24865ed514df78b750e73a502a9eda2c6c7e
ar-ag <aryan070303@gmail.com>
2023-05-31 16:40:54 +0530

    feat: rendered tailor details in tailor profile

--- client/StichHub/src/axios.js
+++ client/StichHub/src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://stichhub-backend.vercel.app",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stichhub-backend.vercel.app",
 });
 
 instance.interceptors.request.use((req) => {
