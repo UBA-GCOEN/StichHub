ee486dfb0475de3610a8070a17e41eaddd200a40
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-31 16:15:04 +0530

    changes-requested

--- client/StichHub/src/components/Home/TailorList.jsx
+++ client/StichHub/src/components/Home/TailorList.jsx
@@ -99,30 +99,10 @@ const TailorList = () => {
                 <span
                   key={id}
                   id="badge-dismiss-default"
-                  className="hidden text-[10px] lg:inline-flex items-center px-1 py-1 mr-2 sm:text-sm font-medium text-blue-800 bg-blue-100 rounded  dark:bg-blue-900 dark:text-blue-300"
+                  className="bg-[#3b82f6] rounded-md px-2 text-white text-[10px] lg:inline-flex items-center py-1 mr-2 sm:text-sm font-medium"
                 >
                   {tags}
-                  <button
-                    type="button"
-                    className="inline-flex items-center p-0.5  text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300"
-                    data-dismiss-target="#badge-dismiss-default"
-                    aria-label="Remove"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      className="w-3.5 h-3.5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                    <span className="sr-only">Remove badge</span>
-                  </button>
+                 
                 </span>
               ))
               .slice(0, 3)}
