2e078bec4ba6d5d5ebd36ae656f2cf59b4fba24f
ar-ag <aryan070303@gmail.com>
2023-05-31 22:12:01 +0530

    fix: fixed merge errors

--- client/StichHub/src/axios.js
+++ client/StichHub/src/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://stichhub-backend.vercel.app",
+    // baseURL: "http://localhost:5000", //! For using local Development, use this URL to connect to your local server.
 });
 
 instance.interceptors.request.use((req) => {
