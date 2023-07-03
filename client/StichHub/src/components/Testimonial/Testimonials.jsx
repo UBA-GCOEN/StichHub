c007518d92a437cad0072829bd8053cdd01e4a7e
Siddhant Patil <shimpli.patil2804@gmail.com>
2023-07-03 09:32:14 +0530

    Update Testimonials.jsx

--- client/StichHub/src/components/Testimonial/Testimonials.jsx
+++ client/StichHub/src/components/Testimonial/Testimonials.jsx
@@ -8,28 +8,28 @@ import hl from "../../assets/hl.jpg";
 const items = [
     {
         user_id: 1,
-        icon: vk,
+        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
         name: "Yadav",
         category: "Customer",
         description: "I stumbled upon this stitching website when I needed to get a custom-made dress for a special occasion, and I couldn't be happier with the result."
     },
     {
         user_id: 2,
-        icon: sp,
+        icon: icon,
         name: "Rishi",
         category: "Tailor",
         description: "As a professional tailor, I have had the opportunity to collaborate with many stitching websites, but this particular website stands out from the rest."
     },
     {
         user_id: 3,
-        icon: nc,
+        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
         name: "Aditya",
         category: "Customer",
         description: "I have been using this stitching website for all my clothing alterations and tailoring needs, and I couldn't be more satisfied. The platform is user-friendly, allowing me to easily upload measurements and instructions for each garment."
     },
     {
         user_id: 4,
-        icon: sy,
+        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
         name: "Rahul",
         category: "Tailor",
         description: "I have been working as a tailor for several years, and I must say that this stitching website has revolutionized the way I collaborate with customers."
@@ -43,7 +43,7 @@ const items = [
     },
     {
         user_id: 6,
-        icon: hl,
+        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
         name: "Vivek",
         category: "Tailor",
         description: "As an experienced tailor, I value efficiency and precision in my work, and this stitching website perfectly aligns with those values."
