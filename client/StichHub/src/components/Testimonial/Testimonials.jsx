92f28e9cd24adaa5c87a846709c6a74126ebd172
Siddhant Patil <shimpli.patil2804@gmail.com>
2023-07-03 09:36:55 +0530

    Update Testimonials.jsx

--- client/StichHub/src/components/Testimonial/Testimonials.jsx
+++ client/StichHub/src/components/Testimonial/Testimonials.jsx
@@ -8,7 +8,7 @@ import hl from "../../assets/hl.jpg";
 const items = [
     {
         user_id: 1,
-        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
+        icon: "https://stichhub.vercel.app/assets/man-da1b7afd.jpg",
         name: "Yadav",
         category: "Customer",
         description: "I stumbled upon this stitching website when I needed to get a custom-made dress for a special occasion, and I couldn't be happier with the result."
@@ -22,14 +22,14 @@ const items = [
     },
     {
         user_id: 3,
-        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
+        icon: "https://img.icons8.com/?size=512&id=492ILERveW8G&format=png",
         name: "Aditya",
         category: "Customer",
         description: "I have been using this stitching website for all my clothing alterations and tailoring needs, and I couldn't be more satisfied. The platform is user-friendly, allowing me to easily upload measurements and instructions for each garment."
     },
     {
         user_id: 4,
-        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
+        icon: "https://stichhub.vercel.app/assets/man-da1b7afd.jpg",
         name: "Rahul",
         category: "Tailor",
         description: "I have been working as a tailor for several years, and I must say that this stitching website has revolutionized the way I collaborate with customers."
@@ -43,7 +43,7 @@ const items = [
     },
     {
         user_id: 6,
-        icon: https://stichhub.vercel.app/assets/man-da1b7afd.jpg,
+        icon: "https://img.icons8.com/?size=512&id=492ILERveW8G&format=png",
         name: "Vivek",
         category: "Tailor",
         description: "As an experienced tailor, I value efficiency and precision in my work, and this stitching website perfectly aligns with those values."
