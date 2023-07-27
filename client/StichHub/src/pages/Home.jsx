eafdbe6c0cc19f3bc2de3e5a16876e9184cf881a
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-07-27 17:34:39 +0530

    issue-367

--- client/StichHub/src/pages/Home.jsx
+++ client/StichHub/src/pages/Home.jsx
@@ -1,43 +1,56 @@
-import {React, useState} from "react";
-import { Navbar, FilterSection, TailorList, Features, CustomerOrder } from "../components/Home/index.js";
+import { React, useState } from "react";
+import {
+  Navbar,
+  FilterSection,
+  TailorList,
+  Features,
+  CustomerOrder,
+} from "../components/Home/index.js";
 import Footer from "../components/MainLandingPage/Footer.jsx";
-
+import Chatbot from "../components/Chatbot.jsx";
 const Home = () => {
-  const [filter, setFilter] = useState({category: false, price: [], rating:[]});
+  const [filter, setFilter] = useState({
+    category: false,
+    price: [],
+    rating: [],
+  });
   const clearFilter = () => {
-    setFilter({category: false, price: [], rating:[]})
+    setFilter({ category: false, price: [], rating: [] });
   };
-  const handleChange = (e)=>{
-    const {name, value} = e.target;
-    if(name !== "category"){
-      if(e.target.checked){
-        setFilter((prev)=>{
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name !== "category") {
+      if (e.target.checked) {
+        setFilter((prev) => {
           return {
             ...prev,
-            [name]: [...filter[name], value] 
-          }
-        })
-      }else{
-        setFilter((prev)=>{
+            [name]: [...filter[name], value],
+          };
+        });
+      } else {
+        setFilter((prev) => {
           return {
             ...prev,
-            [name]: filter[name].filter((item)=>item!==value)
-          }
-        })
+            [name]: filter[name].filter((item) => item !== value),
+          };
+        });
       }
-    }else{
-      setFilter((prev)=>{
-        return {...prev, category: value}
-      })
+    } else {
+      setFilter((prev) => {
+        return { ...prev, category: value };
+      });
     }
-  }
+  };
 
   return (
     <div className="">
       <Navbar />
       <Features />
-      <FilterSection handleChange={handleChange} clearFilter={clearFilter}/>
+      <FilterSection handleChange={handleChange} clearFilter={clearFilter} />
       <TailorList filters={filter} />
+      <div className="">
+        <Chatbot />
+      </div>
       <Footer />
     </div>
   );
